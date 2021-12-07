331e620747ea8ff8f381c39da17f1e925d0639b9
FrancoNatanael <tomainofranco5@gmail.com>
2021-12-07 19:31:37 -0300

    Franco - lista de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,51 @@
+let listaProductos = [
+    {
+        nombre:"Mouse Glorious Model D Minus - Matte Black",
+        categoria:"Periféricos/componentes",
+        precio:"$6.000",
+        codigo:"01",
+        descripcion:"",
+        imagen:"images/mouse.jpg"
+    },
+    {
+        nombre:"Teclado Mecanico Logitech G915 Lightspeed RGB",
+        categoria:"Periféricos/componentes",
+        precio:"$24.590",
+        codigo:"02",
+        descripcion:"",
+        imagen:"images/teclado.jpg"
+    },
+    {
+        nombre:"Silla Gamer HHGears SM-115 Negro Hasta 90kg r",
+        categoria:"Sillas gamer",
+        precio:"$20.000",
+        codigo:"03",
+        descripcion:"",
+        imagen:"images/silla-product.jpg"
+    },
+    {
+        nombre:"Monitor LG 20 20MK400H-B VGA HDMI",
+        categoria:"Pc/Notebook",
+        precio:"$21.950",
+        codigo:"04",
+        descripcion:"",
+        imagen:"images/monitor.jpg"
+    },
+    {
+        nombre:"Auriculares HyperX Cloud Orbit S 7.1 3D Planar",
+        categoria:"Audio",
+        precio:"$29.910",
+        codigo:"05",
+        descripcion:"Auriculares marca Hyper X, en color negro/gris, cuenta con usb, microfono y conectividad Android por Mini Plug,PC,Nintendo Switch,PS4,Xbox One",
+        imagen:"images/auriculares.jpg"
+    }
+
+]
+
 
 const productController = {
     index: (req,res)=>{
-        res.render('products/index')
+        res.render('products/index',{productos:listaProductos})
     },
     edit: (req,res)=>{
         res.render('products/editProduct')
@@ -10,7 +54,7 @@ const productController = {
         res.render('products/productAdd')
     },
     detail: function (req,res) {
-        res.render('products/productDetail')
+        res.render('products/productDetail',{productos:listaProductos})
     },
     cart: (req,res)=>{
         res.render('products/productCart')
